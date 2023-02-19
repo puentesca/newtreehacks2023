795e2252413128124f2670b40269f3caa0dc40c6
Casey Puentes <puentesc121@gmail.com>
2023-02-19 03:02:07 -0800

    Removed OpenXR

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public GameObject testObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OVRInput.Update();
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0.0f && !testObj.activeInHierarchy)
+        {
+            testObj.SetActive(true);
+        } else if (testObj.activeInHierarchy)
+        {
+            testObj.SetActive(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        OVRInput.FixedUpdate();
     }
 }
